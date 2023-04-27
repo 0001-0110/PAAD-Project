dd80dd2ce1520d2e5af8eb8171b559be13af3cd6
Rémi TROTEL <remi.trotel@epita.fr>
2023-04-27 15:16:39 +0200

    Added DbContext

---
+++ PAAD.DAL/DatabaseContext/AufgepasstDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PAAD.DAL.Models;
+
+namespace PAAD.DAL.DatabaseContext
+{
+    internal class AufgepasstDbContext : DbContext
+    {
+        public virtual DbSet<Notification> Notifications { get; set; }
+        public virtual DbSet<Lecturer> Lecturers { get; set; }
+        public virtual DbSet<Course> Courses { get; set; }
+
+        // TODO Get connection string
+        public AufgepasstDbContext() : base(new DbContextOptionsBuilder<AufgepasstDbContext>().UseSqlServer("").Options) { }
+    }
+}
