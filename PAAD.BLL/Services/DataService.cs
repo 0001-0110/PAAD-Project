4edc704c13bce0058145e3c4cae255b1260b284b
Rémi TROTEL <remi.trotel@epita.fr>
2023-05-04 15:12:23 +0200

    Created DataService

---
+++ PAAD.BLL/Services/DataService.cs
@@ -0,0 +1,34 @@
+﻿namespace PAAD.BLL.Services
+{
+    public class DataService
+    {
+        public static readonly DataService Instance = new DataService();
+
+        private DataService() { }
+
+        public T GetAll<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T? GetById<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Create<T>(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Edit(int id, Task entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete<T>(int id) 
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
