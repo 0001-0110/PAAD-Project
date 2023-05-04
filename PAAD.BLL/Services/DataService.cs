7fd1c0c9a1ae20c4818a19425a41405a02923515
Rémi TROTEL <remi.trotel@epita.fr>
2023-05-04 16:15:44 +0200

    Implemented DataService

--- PAAD.BLL/Services/DataService.cs
+++ PAAD.BLL/Services/DataService.cs
@@ -7,31 +7,72 @@ namespace PAAD.BLL.Services
     {
         public static readonly DataService Instance = new DataService();
 
-        private DataService() { }
+        private Dictionary<Type, IRepository<Model>> repositories;
 
-        public T GetAll<T>()
+        private DataService() 
         {
-            throw new NotImplementedException();
+            repositories = new Dictionary<Type, IRepository<Model>>()
+            {
+                [typeof(Notification)] = (IRepository<Model>)new NotificationRepository(),
+                //[typeof(Student)] = (IRepository<Model>)new StudentRepository(),
+                [typeof(Lecturer)] = (IRepository<Model>)new LecturerRepository(),
+                [typeof(Administrator)] = (IRepository<Model>)new AdministratorRepository(),
+                [typeof(Course)] = (IRepository<Model>)new CourseRepository(),
+            };
         }
 
-        public T? GetById<T>()
+        private IRepository<T> GetRepository<T>() where T : Model
         {
-            throw new NotImplementedException();
+            IRepository<Model>? repository;
+
+            if (!repositories.TryGetValue(typeof(T), out repository))
+                throw new KeyNotFoundException();
+
+            return (IRepository<T>)repository;
         }
 
-        public bool Create<T>(T entity)
+        public IEnumerable<T> GetAll<T>() where T : Model
         {
-            throw new NotImplementedException();
+            return GetRepository<T>().GetAll();
         }
 
-        public bool Edit(int id, Task entity)
+        public T? GetById<T>(int id) where T : Model
         {
-            throw new NotImplementedException();
+            return GetRepository<T>().GetById(id);
         }
 
-        public bool Delete<T>(int id) 
+        public bool Create<T>(T entity) where T : Model
         {
-            throw new NotImplementedException();
+            IRepository<T> repository = GetRepository<T>();
+
+            // If there already is an object with the same Id, we can't create it
+            if (repository.IdExists(entity.Id))
+                return false;
+
+            repository.Create(entity);
+            return true;
+        }
+
+        public bool Edit<T>(int id, T entity) where T : Model
+        {
+            IRepository<T> repository = GetRepository<T>();
+
+            if (!repository.IdExists(id))
+                return false;
+
+            repository.Edit(id, entity);
+            return true;
+        }
+
+        public bool Delete<T>(int id) where T : Model
+        {
+            IRepository<T> repository = GetRepository<T>();
+
+            if (!repository.IdExists(id))
+                return false;
+
+            repository.Delete(repository.GetById(id)!);
+            return true;
         }
     }
 }
