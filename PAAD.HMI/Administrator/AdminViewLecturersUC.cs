550bf97c0f11f5e1d43f237258e2c649bdf88a00
Pauline CHARASSON <pauline.charasson@epita.fr>
2023-05-20 01:56:30 +0200

    Forgot to add new files

---
+++ PAAD.HMI/Administrator/AdminViewLecturersUC.cs
@@ -0,0 +1,173 @@
+﻿using InversionOfControl;
+using PAAD.BLL.Services;
+using PAAD.BLL.Utilities;
+using PAAD.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PAAD.HMI.Administrator
+{
+	public partial class AdminViewLecturersUC : UserControl
+	{
+		private readonly IDataService _dataService;
+		private readonly IDependencyInjector _injector;
+		private readonly IAuthenticationService _authenticationService;
+
+		private const string EDIT = "Edit";
+		private const string DELETE = "Delete";
+		public AdminViewLecturersUC(IAuthenticationService authenticationService, IDependencyInjector injector, IDataService dataService)
+		{
+			InitializeComponent();
+			_authenticationService = authenticationService;
+			_injector = injector;
+			_dataService = dataService;
+
+			AddHeader((DAL.Models.Administrator)_authenticationService.CurrentUser!);
+		}
+
+		private void AddHeader(DAL.Models.Administrator user)
+		{
+			AdminHeaderUC headerUC = new AdminHeaderUC();
+			//HeaderUC headerUC = new HeaderUC(IDataService dataService)
+			headerUC.DisplayUser(user);
+		}
+
+		private void AdminViewLecturersUC_Load(object sender, EventArgs e)
+		{
+
+			try
+			{
+				Course course;
+				IEnumerable<DAL.Models.Lecturer> lecturers = _dataService.GetAll<DAL.Models.Lecturer>();
+				foreach (var lecturer in lecturers)
+				{
+					course = _dataService.GetById<Course>((int)lecturer.CourseId!)!;
+					dataGridView_Lecturer.Rows.Add($"{lecturer.FirstName} {lecturer.LastName}", course.Name, EDIT, DELETE);
+				}
+			}
+			catch (Exception ex)
+			{
+				Utils.ShowError(ex.Message);
+				Environment.Exit(1);
+			}
+		}
+
+		private void btnAddLecturer_Click(object sender, EventArgs e)
+		{
+			AddLecturerForm addLecturerForm = _injector.Instantiate<AddLecturerForm>("Add")!;
+			if (addLecturerForm.ShowDialog() == DialogResult.OK)
+			{
+				var lecturer = addLecturerForm.GetData();
+				var course = _dataService.GetById<Course>((int)lecturer.CourseId!)!;
+
+				//save into database
+				_dataService.Create<DAL.Models.Lecturer>(lecturer);
+
+				//adds in the table
+				dataGridView_Lecturer.Rows.Add($"{lecturer.FirstName} {lecturer.LastName}", course.Name, EDIT, DELETE);
+
+			}
+		}
+
+
+		private void btnBack_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				AdminHomeUC adminHomeUC = _injector.Instantiate<AdminHomeUC>()!;
+
+				adminHomeUC.Dock = DockStyle.Fill;
+
+				Parent.Controls.Add(adminHomeUC);
+				Parent.Controls.Remove(this);
+
+				this.Dispose();
+			}
+			catch (Exception ex)
+			{
+				Utils.ShowError(ex.Message);
+				Environment.Exit(1);
+			}
+		}
+
+		private void dataGridView_Lecturer_CellContentClick(object sender, DataGridViewCellEventArgs e)
+		{
+			var senderGrid = (DataGridView)sender;
+
+			if (senderGrid.Columns[e.ColumnIndex] is DataGridViewButtonColumn &&
+				e.RowIndex >= 0)
+			{
+				if (e.ColumnIndex == 2)
+					Edit_Click(sender, e);
+				else
+					Delete_Click(sender, e);
+			}
+		}
+
+		private void Delete_Click(object sender, DataGridViewCellEventArgs e)
+		{
+			try
+			{
+				var senderGrid = (DataGridView)sender;
+
+				var row = senderGrid.SelectedRows[0];
+
+				var lecturer = _dataService.GetAll<DAL.Models.Lecturer>().FirstOrDefault(l => $"{l.FirstName} {l.LastName}" == row.Cells[0].Value.ToString())!;
+				var id = lecturer.Id;
+
+				//delete row
+				dataGridView_Lecturer.Rows.Remove(row);
+
+				//delete in the database
+				_dataService.Delete<DAL.Models.Lecturer>(id);
+			}
+			catch (Exception ex)
+			{
+				Utils.ShowError(ex.Message);
+				Environment.Exit(1);
+			}
+		}
+
+		private void Edit_Click(object sender, DataGridViewCellEventArgs e)
+		{
+
+			try
+			{
+				var senderGrid = (DataGridView)sender;
+
+				var row = senderGrid.SelectedRows[0]; //we know there is only one max
+
+				var lecturer = _dataService.GetAll<DAL.Models.Lecturer>().FirstOrDefault(l => $"{l.FirstName} {l.LastName}" == row.Cells[0].Value.ToString())!;
+				var id = lecturer.Id;
+
+				AddLecturerForm addLecturerForm = _injector.Instantiate<AddLecturerForm>("Edit", lecturer)!;
+
+				if (addLecturerForm.ShowDialog() == DialogResult.OK)
+				{
+					lecturer = addLecturerForm.GetData();
+
+					//update database
+					_dataService.Edit<DAL.Models.Lecturer>(id, lecturer);
+
+					//update table
+					row.Cells[0].Value = $"{lecturer.FirstName} {lecturer.LastName}";
+					row.Cells[1].Value = _dataService.GetById<Course>((int)lecturer.CourseId!);
+				}
+
+			}
+			catch (Exception ex)
+			{
+				Utils.ShowError(ex.Message);
+				Environment.Exit(1);
+			}
+		}
+	}
+}
